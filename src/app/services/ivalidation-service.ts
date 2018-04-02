d03522b6ce425aa0761a181b15bc262d1c4613a7
VeritasSoftware <shantanu_negi@hotmail.com>
2018-04-02 20:03:06 +1000

    added method to IValidationService interface.

--- src/app/services/ivalidation-service.ts
+++ src/app/services/ivalidation-service.ts
@@ -1,6 +1,7 @@
-import { User } from '../models/models.component'
+import { User, RegisterUser } from '../models/models.component'
 import { ValidationResult } from '../core/validate';
 
 export interface IValidationService {
     validateUser(model: User) : ValidationResult;
+    validateRegisterUser(model: RegisterUser) : ValidationResult;
 }
